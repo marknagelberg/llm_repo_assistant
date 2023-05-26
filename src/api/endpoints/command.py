758a46624cf77ab6b282b499ee57606bc270b46b
macbook <nagelbergmark@gmail.com>
2023-05-26 05:28:40 -0500

    Fix bug where command configuration could not handle commands with no args or flags

--- src/api/endpoints/command.py
+++ src/api/endpoints/command.py
@@ -34,17 +34,19 @@ def create_router_for_command(command: Command) -> APIRouter:
     # Field(Query()) is used as only using Field() will not allow the field description to be added.
     # Seems to be an issue in Pydantic or FastAPI when handling dynamically created models. 
     fields = {}
-    for arg in command.args:
-        python_friendly_arg_name = clean_name(arg.name)
-        fields[python_friendly_arg_name] = (str, Field(Query(..., description=arg.description))) if not arg.optional else (Optional[str], Field(Query(None, description=arg.description)))
-    
-    for flag in command.flags:
-        if flag.optional:  # Only optional flags are added as fields
-            python_friendly_flag_name = clean_name(flag.name)
-            if flag.type == "bool":
-                fields[python_friendly_flag_name] = (bool, Field(Query(False, description="Flag: " + flag.description)))
-            if flag.type == "str":
-                fields[python_friendly_flag_name] = (str, Field(Query("", description="Flag: " + flag.description)))
+    if command.args is not None:
+        for arg in command.args:
+            python_friendly_arg_name = clean_name(arg.name)
+            fields[python_friendly_arg_name] = (str, Field(Query(..., description=arg.description))) if not arg.optional else (Optional[str], Field(Query(None, description=arg.description)))
+        
+    if command.flags is not None:
+        for flag in command.flags:
+            if flag.optional:  # Only optional flags are added as fields
+                python_friendly_flag_name = clean_name(flag.name)
+                if flag.type == "bool":
+                    fields[python_friendly_flag_name] = (bool, Field(Query(False, description="Flag: " + flag.description)))
+                if flag.type == "str":
+                    fields[python_friendly_flag_name] = (str, Field(Query("", description="Flag: " + flag.description)))
 
     CommandModel = create_model(command.name.capitalize(), **fields)
 
@@ -55,36 +57,37 @@ def create_router_for_command(command: Command) -> APIRouter:
         command_line = [command.command]
 
         # Add arguments and flags to the command line
-        for arg in command.args:
-            python_friendly_arg_name = clean_name(arg.name)
-            value = getattr(command_model, python_friendly_arg_name, None)
-            if value is not None:
-                if arg.is_directory_or_file:
-                    command_line.extend([get_filesystem_path(str(value))])
-                else:
-                    command_line.extend([str(value)])
+        if command.args is not None:
+            for arg in command.args:
+                python_friendly_arg_name = clean_name(arg.name)
+                value = getattr(command_model, python_friendly_arg_name, None)
+                if value is not None:
+                    if arg.is_directory_or_file:
+                        command_line.extend([get_filesystem_path(str(value))])
+                    else:
+                        command_line.extend([str(value)])
                     
-
-        for flag in command.flags:
-            python_friendly_flag_name = clean_name(flag.name)
-            if not flag.optional:  # Required flags are added automatically
-                if flag.is_short:
-                    flag_str = f"-{flag.name}"
-                else:
-                    flag_str = f"--{flag.name}"
-                command_line.append(flag_str)
-            else:  # Optional flags are added only if their value is True
-                value = getattr(command_model, python_friendly_flag_name, None)
-                if value:
+        if command.flags is not None:
+            for flag in command.flags:
+                python_friendly_flag_name = clean_name(flag.name)
+                if not flag.optional:  # Required flags are added automatically
                     if flag.is_short:
                         flag_str = f"-{flag.name}"
                     else:
                         flag_str = f"--{flag.name}"
-                    if flag.type == "str":  # Use the provided value as the flag's value
-                        flag_value = str(value)
-                        command_line.append(f"{flag_str}={flag_value}")
-                    else:
-                        command_line.append(flag_str)
+                    command_line.append(flag_str)
+                else:  # Optional flags are added only if their value is True
+                    value = getattr(command_model, python_friendly_flag_name, None)
+                    if value:
+                        if flag.is_short:
+                            flag_str = f"-{flag.name}"
+                        else:
+                            flag_str = f"--{flag.name}"
+                        if flag.type == "str":  # Use the provided value as the flag's value
+                            flag_value = str(value)
+                            command_line.append(f"{flag_str}={flag_value}")
+                        else:
+                            command_line.append(flag_str)
 
         try:
             exit_code, output_str = run_command_in_image(settings.TARGET_REPO_DOCKER_IMAGE_NAME, command_line)
